bba18e8bf8730b1abd00eddb244bff0cef3be4bf
Gábor Görzsöny <gabor@gorzsony.com>
2024-03-12 14:37:17 +0100

    Added more tests

--- broadcaster_test.go
+++ broadcaster_test.go
@@ -1,10 +1,12 @@
 package broadcaster
 
 import (
+	"context"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestBroadcast(t *testing.T) {
@@ -15,9 +17,11 @@ func TestBroadcast(t *testing.T) {
 
 	l1, _, err := b.Listen()
 	assert.NoError(t, err)
+	require.NotNil(t, l1)
 
 	l2, _, err := b.Listen()
 	assert.NoError(t, err)
+	require.NotNil(t, l2)
 
 	for i := 1; i <= numMessages; i++ {
 		ch <- i
@@ -43,6 +47,7 @@ func TestTimeout(t *testing.T) {
 
 	l, _, err := b.Listen()
 	assert.NoError(t, err)
+	require.NotNil(t, l)
 
 	ch <- 1
 	assert.Equal(t, 1, <-l)
@@ -53,18 +58,62 @@ func TestTimeout(t *testing.T) {
 	assert.False(t, ok)
 }
 
+func TestBlocking(t *testing.T) {
+	ch := make(chan int)
+	b := NewBroadcaster(ch, WithBlocking(true))
+
+	select {
+	case ch <- 1:
+		t.Fatal("channel should be blocked")
+	default:
+	}
+
+	l, _, err := b.Listen(WithBufferSize(1))
+	assert.NoError(t, err)
+	require.NotNil(t, l)
+
+	select {
+	case ch <- 1:
+	default:
+		t.Fatal("channel should not block")
+	}
+}
+
 func TestListenerClose(t *testing.T) {
 	ch := make(chan int, 1)
 	b := NewBroadcaster(ch)
 
-	l, close, err := b.Listen()
+	l, cancel, err := b.Listen()
 	assert.NoError(t, err)
+	require.NotNil(t, l)
+	require.NotNil(t, cancel)
 
 	ch <- 1
 	assert.Equal(t, 1, <-l)
 
-	close()
+	cancel()
 	ch <- 2
 	_, ok := <-l
+	assert.False(t, ok)
+}
+
+func TestListenerContext(t *testing.T) {
+	ch := make(chan int)
+	b := NewBroadcaster(ch)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	l, _, err := b.Listen(WithContext(ctx), WithBufferSize(1))
+	assert.NoError(t, err)
+	require.NotNil(t, l)
+
+	ch <- 1
+
+	res, ok := <-l
+	assert.Equal(t, 1, res)
+	assert.True(t, ok)
+
+	cancel()
+
+	_, ok = <-l
 	assert.False(t, ok)
 }
