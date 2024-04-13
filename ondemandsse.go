7fe7954edbdb924c7bb5f7140a509312ce3a87c6
Gábor Görzsöny <gabor@gorzsony.com>
2024-04-13 13:31:32 +0200

    Simplified on-demand SSE broadcaster

--- ondemandsse.go
+++ ondemandsse.go
@@ -3,29 +3,23 @@ package broadcaster
 import (
 	"context"
 	"net/http"
-	"sync"
 )
 
 type OndemandEventSource func() (EventSource, error)
 
 func NewOndemandSSEBroadcaster(src OndemandEventSource, opts ...BroadcasterOption) http.Handler {
-	var mu sync.Mutex
-	var b Broadcaster[string]
-
-	listen := func(ctx context.Context) (<-chan string, error) {
-		mu.Lock()
-		defer mu.Unlock()
-		if b == nil || b.IsClosed() {
-			evSrc, err := src()
-			if err != nil {
-				return nil, err
-			}
-			b = NewConverterBroadcaster(evSrc.run(), marshalEvent, opts...)
+	source := func() (<-chan event, error) {
+		src, err := src()
+		if err != nil {
+			return nil, err
 		}
+		return src.run(), nil
+	}
+	b := NewOndemandConverterBroadcaster(source, marshalEvent, opts...)
+	listen := func(ctx context.Context) (<-chan string, error) {
 		l, _, err := b.Listen(WithContext(ctx))
 		return l, err
 	}
-
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		serveSSE(listen, w, r)
 	})
